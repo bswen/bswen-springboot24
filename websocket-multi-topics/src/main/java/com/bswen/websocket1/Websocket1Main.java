69a008e6de10a1d2c3fbddaa5321443c4ce50f22
zzz <zzz@zzz.com>
2021-05-11 13:59:26 +0800

    add1

---
+++ websocket-multi-topics/src/main/java/com/bswen/websocket1/Websocket1Main.java
@@ -0,0 +1,11 @@
+package com.bswen.websocket1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Websocket1Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Websocket1Main.class,args);
+    }
+}
